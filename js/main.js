defa8bd0ee77e7640b83f84a22151b32568023f3
Reem-Nosbaum <reemnu@gmail.com>
2023-08-07 21:15:04 +0300

    Improve structure / format of the code. :art:

--- js/main.js
+++ js/main.js
@@ -1,46 +1,8 @@
 var map = L.map('map')
-map.setView([0, 0], 5);
+map.setView([32.0790096,34.7680621], 13);
 
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '© OpenStreetMap'
 }).addTo(map);
-
-const curLocation = navigator.geolocation.watchPosition(succsess, error)
-let marker, circle, zoomed
-function succsess(pos) {
-
-    const lat = pos.coords.latitude
-    const lng = pos.coords.longitude
-    const accuracy = pos.coords.accuracy
-    console.log(lat);
-
-    if(marker) {
-        map.removeLayer(marker)
-        map.removeLayer(circle)
-
-    }
-
-    marker = L.marker([lat, lng]).addTo(map)
-    circle = L.circle( [lat, lng], { radius: accuracy }).addTo(map)
-
-
-    if(!zoomed) {         
-    zoomed = map.fitBounds(circle.getBounds())
-    }
-
-    map.setView([lat, lng])
-}
-
-function error(err) {
-
-    if (err.code === 1) {
-        alert("Please allow geolocation access");
-        // Runs if user refuses access
-    } else {
-        alert("Cannot get current location");
-        // Runs if there was a technical problem.
-    }
-
-}
