d41494a7e28a136f255519b1529b09a67c361468
Reem-Nosbaum <reemnu@gmail.com>
2023-08-10 18:05:21 +0300

    Improve structure / format of the code. :art:

--- js/main.js
+++ js/main.js
@@ -1,26 +1,40 @@
-let coordes;
-let map;             // renanim                 green kfer saba         Ayalon Mall
-const toiletArry = [[32.1974516, 34.8732257], [32.1920517,34.8891483], [32.100446,34.8240358]];
+let coords;
+let map;
+const toiletArry = [
+    { coords: [32.1974516, 34.8732257], label: 'renanim' },
+    { coords: [32.1920517, 34.8891483], label: 'green kfer saba' },
+    { coords: [32.100446, 34.8240358], label: 'Ayalon Mall' },
+];
 let markers = [];
+
+function GoBackToMyLocation() {
+    const myLocationMarker = L.marker(coords).addTo(map);
+    myLocationMarker.bindPopup("<b>My Location</b>").openPopup();
+    
+    map.setView(coords, 16);
+}
+
 
 function toiletLocations() {
     // Clear previous markers
     markers.forEach(marker => marker.removeFrom(map));
 
     // Add new markers
-    toiletArry.forEach(coords => {
+    toiletArry.forEach(entry => {
+        const { coords, label } = entry;
         const marker = L.marker(coords).addTo(map);
+        marker.bindPopup(label);
         markers.push(marker);
     });
 
     // Find nearest marker and move map to it
-    if (coordes) {
+    if (coords) {
         let nearestMarker = null;
         let nearestDistance = Infinity;
 
         markers.forEach(marker => {
             const markerCoords = marker.getLatLng();
-            const distance = coordes.distanceTo(markerCoords);
+            const distance = coords.distanceTo(markerCoords);
 
             if (distance < nearestDistance) {
                 nearestMarker = marker;
@@ -29,18 +43,20 @@ function toiletLocations() {
         });
 
         if (nearestMarker) {
-            map.setView(nearestMarker.getLatLng(), 15);
+            map.setView(nearestMarker.getLatLng(), 16);
+            nearestMarker.openPopup();
         }
     }
 }
+
 
 if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function (position) {
         const { latitude } = position.coords;
         const { longitude } = position.coords;
-        coordes = L.latLng(latitude, longitude);
+        coords = L.latLng(latitude, longitude);
 
-        map = L.map('map').setView(coordes, 13);
+        map = L.map('map').setView(coords, 15);
         L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
             maxZoom: 19,
             attribution: '© OpenStreetMap'
