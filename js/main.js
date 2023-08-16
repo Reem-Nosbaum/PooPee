51b9a8253abc2119126c66f48820d0514c9868f5
Reem-Nosbaum <reemnu@gmail.com>
2023-08-16 18:46:56 +0300

    Improve structure / format of the code. :art:

--- js/main.js
+++ js/main.js
@@ -1,17 +1,26 @@
 let coords;
 let map;
+let toiletMarkers = []; // Array to store the toilet markers
+let myLocationMarker = null; // Variable to store the My Location marker
+
 const toiletArry = [
     { coords: [32.1974516, 34.8732257], label: 'renanim' },
     { coords: [32.1920517, 34.8891483], label: 'green kfer saba' },
     { coords: [32.100446, 34.8240358], label: 'Ayalon Mall' },
 ];
-let markers = [];
 
 
-function GoBackToMyLocation() {
-    const myLocationMarker = L.marker(coords).addTo(map);
+function myLocation() {
+    // Remove the existing My Location marker from the map
+    if (myLocationMarker) {
+        map.removeLayer(myLocationMarker);
+        myLocationMarker = null;
+    }
+
+    // Add a new My Location marker to the map
+    myLocationMarker = L.marker(coords).addTo(map);
     myLocationMarker.bindPopup("<b>My Location</b>").openPopup();
-    
+
     map.setView(coords, 16);
 }
 
@@ -20,46 +29,21 @@ function toggleFooter() {
     footer.classList.toggle('hidden');
 }
 
-function toiletLocations() {
-    // Clear previous markers
-    markers.forEach(marker => marker.removeFrom(map));
 
-    toiletArry.forEach(e => {
-        const marker = L.marker(e.coords).addTo(map);
-        marker.bindPopup(e.label);
-        marker.on('click', () => {
-            toggleFooter(); // Toggle the footer visibility on marker click
-            // Set up event listener for the "Directions" button click
-        const directionsButton = document.querySelector('.btn-navigation');
-        directionsButton.addEventListener('click', () => {
-            openNavigationInWaze(e.coords, e.label); // Pass the coordinates and label
-        });
-        });
-        markers.push(marker);
-        console.log(markers);
+function toiletLocation() {
+    // Remove existing toilet markers from the map
+    toiletMarkers.forEach(markerEl => map.removeLayer(markerEl));
+    toiletMarkers = []; // Clear the array
+
+    // Add new toilet markers to the map
+    toiletArry.forEach((markerEl) => {
+        const marker = L.marker(markerEl.coords).addTo(map).bindPopup(markerEl.label).openPopup();
+        toiletMarkers.push(marker); // Store the marker in the array
+        map.setView(markerEl.coords, 16);
+
     });
-
-    // Find nearest marker and move map to it
-    if (coords) {
-        let nearestMarker = null;
-        let nearestDistance = Infinity;
-
-        markers.forEach(marker => {
-            const markerCoords = marker.getLatLng();
-            const distance = coords.distanceTo(markerCoords);
-
-            if (distance < nearestDistance) {
-                nearestMarker = marker;
-                nearestDistance = distance;
-            }
-        });
-
-        if (nearestMarker) {
-            map.setView(nearestMarker.getLatLng(), 16);
-            nearestMarker.openPopup();
-        }
-    }
 }
+
 if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function (position) {
         const { latitude } = position.coords;
@@ -79,15 +63,4 @@ if (navigator.geolocation) {
     });
 }
 
-function openNavigationInWaze(coords) {
-    console.log(coords);
-
-    // alert('you abuot to leav this page to waze')
-    // Create the Waze URL with the specified latitude and longitude
-    const wazeUrl = `https://www.waze.com/ul?ll=${coords[0]},${coords[1]}&navigate=yes`;
-
-    // Open the link in a new tab or window
-    window.open(wazeUrl, '_blank');
-
-}
 
